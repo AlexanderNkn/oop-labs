f77f36037b1209e474601f0ad40886059d188b7a
AlexanderNkn <yspresi@yandex.ru>
2024-09-18 01:40:04 +0300

    added lab5

--- src/main/java/lab5/StreamApiConsoleApp.java
+++ src/main/java/lab5/StreamApiConsoleApp.java
@@ -18,7 +18,6 @@ public class StreamApiConsoleApp extends JFrame{
     List<String> list7;
 
     public StreamApiConsoleApp() {
-        super("lab_5");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 700, 600);
         JPanel contentPane = new JPanel();
@@ -26,14 +25,11 @@ public class StreamApiConsoleApp extends JFrame{
         contentPane.setLayout(null);
 
         //Task 1
-
-        JLabel task1Label = new JLabel("Task 1:");
-        task1Label.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task1Label = new JLabel("Задача 1:");
         task1Label.setBounds(27, 60, 250, 23);
         contentPane.add(task1Label);
 
-        JLabel task1InputDatalabel = new JLabel("Input data:");
-        task1InputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task1InputDatalabel = new JLabel("Исходные данные:");
         task1InputDatalabel.setBounds(27, 83, 250, 23);
         contentPane.add(task1InputDatalabel);
 
@@ -43,8 +39,7 @@ public class StreamApiConsoleApp extends JFrame{
         task1InputDataField.setEditable(false);
         contentPane.add(task1InputDataField);
 
-        JLabel task1OutputDatalabel = new JLabel("Output data:");
-        task1OutputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task1OutputDatalabel = new JLabel("Результат:");
         task1OutputDatalabel.setBounds(27, 129, 250, 23);
         contentPane.add(task1OutputDatalabel);
 
@@ -55,42 +50,36 @@ public class StreamApiConsoleApp extends JFrame{
         contentPane.add(task1OutputDataField);
 
         //Task 2
-        JLabel task2Label = new JLabel("Task 2:");
-        task2Label.setBounds(327, 60, 250, 23);
-        task2Label.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task2Label = new JLabel("Задача 2:");
+        task2Label.setBounds(327, 60, 350, 23);
         contentPane.add(task2Label);
 
-        JLabel task2InputDatalabel = new JLabel("Input data:");
-        task2InputDatalabel.setBounds(327, 83, 250, 23);
-        task2InputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task2InputDatalabel = new JLabel("Исходные данные:");
+        task2InputDatalabel.setBounds(327, 83, 350, 23);
         contentPane.add(task2InputDatalabel);
 
         JTextField task2InputDataField = new JTextField();
-        task2InputDataField.setBounds(327, 106, 250, 23);
+        task2InputDataField.setBounds(327, 106, 350, 23);
         task2InputDataField.setColumns(500);
         task2InputDataField.setEditable(false);
         contentPane.add(task2InputDataField);
 
-        JLabel task2OutputDatalabel = new JLabel("Output data:");
-        task2OutputDatalabel.setBounds(327, 129, 250, 23);
-        task2OutputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task2OutputDatalabel = new JLabel("Результат:");
+        task2OutputDatalabel.setBounds(327, 129, 350, 23);
         contentPane.add(task2OutputDatalabel);
 
         JTextField task2OutputDataField = new JTextField();
-        task2OutputDataField.setBounds(327, 152, 250, 23);
+        task2OutputDataField.setBounds(327, 152, 350, 23);
         task2OutputDataField.setColumns(500);
         task2OutputDataField.setEditable(false);
         contentPane.add(task2OutputDataField);
 
-
         //Task 3
-        JLabel task3Label = new JLabel("Task 3:");
-        task3Label.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task3Label = new JLabel("Задача 3:");
         task3Label.setBounds(27, 176, 250, 23);
         contentPane.add(task3Label);
 
-        JLabel task3InputDatalabel = new JLabel("Input data:");
-        task3InputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task3InputDatalabel = new JLabel("Исходные данные:");
         task3InputDatalabel.setBounds(27, 199, 250, 23);
         contentPane.add(task3InputDatalabel);
 
@@ -100,8 +89,7 @@ public class StreamApiConsoleApp extends JFrame{
         task3InputDataField.setEditable(false);
         contentPane.add(task3InputDataField);
 
-        JLabel task3OutputDatalabel = new JLabel("Output data:");
-        task3OutputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task3OutputDatalabel = new JLabel("Результат:");
         task3OutputDatalabel.setBounds(27, 245, 250, 23);
         contentPane.add(task3OutputDatalabel);
 
@@ -112,41 +100,36 @@ public class StreamApiConsoleApp extends JFrame{
         contentPane.add(task3OutputDataField);
 
         //Task 4
-        JLabel task4Label = new JLabel("Task 4:");
-        task4Label.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        task4Label.setBounds(327, 176, 250, 23);
+        JLabel task4Label = new JLabel("Задача 4:");
+        task4Label.setBounds(327, 176, 350, 23);
         contentPane.add(task4Label);
 
-        JLabel task4InputDatalabel = new JLabel("Input data:");
-        task4InputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        task4InputDatalabel.setBounds(327, 199, 250, 23);
+        JLabel task4InputDatalabel = new JLabel("Исходные данные:");
+        task4InputDatalabel.setBounds(327, 199, 350, 23);
         contentPane.add(task4InputDatalabel);
 
         JTextField task4InputDataField = new JTextField();
-        task4InputDataField.setBounds(327, 222, 250, 23);
+        task4InputDataField.setBounds(327, 222, 350, 23);
         task4InputDataField.setColumns(500);
         task4InputDataField.setEditable(false);
         contentPane.add(task4InputDataField);
 
-        JLabel task4OutputDatalabel = new JLabel("Output data:");
-        task4OutputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        task4OutputDatalabel.setBounds(327, 245, 250, 23);
+        JLabel task4OutputDatalabel = new JLabel("Результат:");
+        task4OutputDatalabel.setBounds(327, 245, 350, 23);
         contentPane.add(task4OutputDatalabel);
 
         JTextField task4OutputDataField = new JTextField();
-        task4OutputDataField.setBounds(327, 268, 250, 23);
+        task4OutputDataField.setBounds(327, 268, 350, 23);
         task4OutputDataField.setColumns(500);
         task4OutputDataField.setEditable(false);
         contentPane.add(task4OutputDataField);
 
         //Task 5
-        JLabel task5Label = new JLabel("Task 5:");
-        task5Label.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task5Label = new JLabel("Задача 5:");
         task5Label.setBounds(27, 291, 250, 23);
         contentPane.add(task5Label);
 
-        JLabel task5InputDatalabel = new JLabel("Input data:");
-        task5InputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task5InputDatalabel = new JLabel("Исходные данные:");
         task5InputDatalabel.setBounds(27, 314, 250, 23);
         contentPane.add(task5InputDatalabel);
 
@@ -156,8 +139,7 @@ public class StreamApiConsoleApp extends JFrame{
         task5InputDataField.setEditable(false);
         contentPane.add(task5InputDataField);
 
-        JLabel task5OutputDatalabel = new JLabel("Output data:");
-        task5OutputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        JLabel task5OutputDatalabel = new JLabel("Результат:");
         task5OutputDatalabel.setBounds(27, 360, 250, 23);
         contentPane.add(task5OutputDatalabel);
 
@@ -168,72 +150,66 @@ public class StreamApiConsoleApp extends JFrame{
         contentPane.add(task5OutputDataField);
 
         //Task 6
-        JLabel task6Label = new JLabel("Task 6:");
-        task6Label.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        task6Label.setBounds(327, 291, 250, 23);
+        JLabel task6Label = new JLabel("Задача 6:");
+        task6Label.setBounds(327, 291, 350, 23);
         contentPane.add(task6Label);
 
-        JLabel task6InputDatalabel = new JLabel("Input data:");
-        task6InputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        task6InputDatalabel.setBounds(327, 314, 250, 23);
+        JLabel task6InputDatalabel = new JLabel("Исходные данные:");
+        task6InputDatalabel.setBounds(327, 314, 350, 23);
         contentPane.add(task6InputDatalabel);
 
         JTextField task6InputDataField = new JTextField();
-        task6InputDataField.setBounds(327, 337, 250, 23);
+        task6InputDataField.setBounds(327, 337, 350, 23);
         task6InputDataField.setColumns(500);
         task6InputDataField.setEditable(false);
         contentPane.add(task6InputDataField);
 
-        JLabel task6OutputDatalabel = new JLabel("Output data:");
-        task6OutputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        task6OutputDatalabel.setBounds(327, 360, 250, 23);
+        JLabel task6OutputDatalabel = new JLabel("Результат:");
+        task6OutputDatalabel.setBounds(327, 360, 350, 23);
         contentPane.add(task6OutputDatalabel);
 
         JTextField task6OutputDataField = new JTextField();
-        task6OutputDataField.setBounds(327, 383, 250, 23);
+        task6OutputDataField.setBounds(327, 383, 350, 23);
         task6OutputDataField.setColumns(500);
         task6OutputDataField.setEditable(false);
         contentPane.add(task6OutputDataField);
 
 
         //Task 7
-        JLabel task7Label = new JLabel("Task 7:");
-        task7Label.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        task7Label.setBounds(27, 406, 250, 23);
+        JLabel task7Label = new JLabel("Задача 7:");
+        task7Label.setBounds(27, 406, 350, 23);
         contentPane.add(task7Label);
 
-        JLabel task7InputDatalabel = new JLabel("Input data:");
-        task7InputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        task7InputDatalabel.setBounds(27, 429, 250, 23);
+        JLabel task7InputDatalabel = new JLabel("Исходные данные:");
+        task7InputDatalabel.setBounds(27, 429, 350, 23);
         contentPane.add(task7InputDatalabel);
 
         JTextField task7InputDataField = new JTextField();
-        task7InputDataField.setBounds(27, 452, 250, 23);
+        task7InputDataField.setBounds(27, 452, 350, 23);
         task7InputDataField.setColumns(500);
         task7InputDataField.setEditable(false);
         contentPane.add(task7InputDataField);
 
-        JLabel task7OutputDatalabel = new JLabel("Output data:");
-        task7OutputDatalabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        task7OutputDatalabel.setBounds(27, 475, 250, 23);
+        JLabel task7OutputDatalabel = new JLabel("Результат:");
+        task7OutputDatalabel.setBounds(27, 475, 350, 23);
         contentPane.add(task7OutputDatalabel);
 
         JTextField task7OutputDataField = new JTextField();
-        task7OutputDataField.setBounds(27, 498, 250, 23);
+        task7OutputDataField.setBounds(27, 498, 350, 23);
         task7OutputDataField.setColumns(500);
         task7OutputDataField.setEditable(false);
         contentPane.add(task7OutputDataField);
 
-        JButton btnGetInputData = new JButton("Get input data");
+        JButton btnGetInputData = new JButton("Сгенерить исходные данные");
         btnGetInputData.addActionListener(arg0 -> {
             if(arg0.getSource() == btnGetInputData) {
                 list1 = getIntegerList();
-                list2 = getStringList();
+                list2 = getStringList('z');
                 list3 =  getIntegerList();
-                list4 = getStringList();
+                list4 = getStringList('c');
                 list5 = getIntegerList();
                 a6 = getIntegerArray();
-                list7 = getStringList();
+                list7 = getStringList('z');
 
                 task1InputDataField.setText(list1.toString());
                 task2InputDataField.setText(list2.toString());
@@ -249,12 +225,12 @@ public class StreamApiConsoleApp extends JFrame{
         btnGetInputData.setBounds(27, 20, 600, 40);
         contentPane.add(btnGetInputData);
 
-        JButton btnGetResult = new JButton("Get result");
+        JButton btnGetResult = new JButton("Рассчитать");
         btnGetResult.addActionListener(arg0 -> {
             if(arg0.getSource() == btnGetResult) {
                 task1OutputDataField.setText(String.valueOf(StreamApiMethods.getAverageValue(list1)));
                 task2OutputDataField.setText(String.valueOf(StreamApiMethods.getUppercaseStrings(list2)));
-                task3OutputDataField.setText(String.valueOf(StreamApiMethods.getListSquares(list3)));
+                task3OutputDataField.setText(String.valueOf(StreamApiMethods.getSquaresList(list3)));
                 task4OutputDataField.setText(String.valueOf(StreamApiMethods.getSortedStrings(list4, "a")));
                 try {
                     task5OutputDataField.setText(String.valueOf(StreamApiMethods.getLastElementOfCollection(list5)));
@@ -267,21 +243,21 @@ public class StreamApiConsoleApp extends JFrame{
                 try {
                     task7OutputDataField.setText(String.valueOf(StreamApiMethods.getStringMap(list7)));
                 } catch (IllegalStateException e) {
-                    task7OutputDataField.setText("Duplicate key v (attempted merging values");
+                    task7OutputDataField.setText("Дублируется ключ");
                 }
             }
             setVisible(true);
         });
-        btnGetResult.setBounds(327, 406, 250, 115);
+        btnGetResult.setBounds(527, 456, 150, 60);
         contentPane.add(btnGetResult);
 
-        JButton btnExit = new JButton("Exit");
+        JButton btnExit = new JButton("Выход");
         btnExit.addActionListener((ActionEvent e) -> {
             JComponent comp = (JComponent) e.getSource();
             Window win = SwingUtilities.getWindowAncestor(comp);
             win.dispose();
         });
-        btnExit.setBounds(502, 525, 75, 23);
+        btnExit.setBounds(582, 525, 95, 23);
         contentPane.add(btnExit);
     }
     public static void main(String[] args) {
@@ -313,11 +289,11 @@ public class StreamApiConsoleApp extends JFrame{
         return result;
     }
 
-    private static List<String> getStringList() {
+    private static List<String> getStringList(char lastLetter) {
         return Stream.generate(() -> {
-                    int length = new Random().nextInt(10) + 1;
+                    int length = new Random().nextInt(5) + 1;
                     StringBuilder sb = new StringBuilder();
-                    new Random().ints(length, 'a', 'z' + 1)
+                    new Random().ints(length, 'a', lastLetter + 1)
                             .forEach(randomChar -> sb.append((char) randomChar));
                     return sb.toString();
                 })
--- src/main/java/lab5/StreamApiMethods.java
+++ src/main/java/lab5/StreamApiMethods.java
@@ -5,7 +5,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StreamApiMethods {
-    //Task 1
+    // Task 1
+    // метод, возвращающий среднее значение списка целых чисел
     public static double getAverageValue(List<Integer> list) {
         return list.stream()
                 .mapToInt(Integer::intValue)
@@ -13,15 +14,19 @@ public class StreamApiMethods {
                 .getAverage();
     }
 
-    //Task 2
+    // Task 2
+    // метод, приводящий все строки в списке в верхний регистр и
+    // добавляющий к ним префикс «_new_»
     public static List<String> getUppercaseStrings(List<String> list) {
         return list.stream()
-                .map(str -> str = "_new_" + str.toUpperCase())
+                .map(str -> "_new_" + str.toUpperCase())
                 .toList();
     }
 
-    //Task 3
-    public static List<Integer> getListSquares(List<Integer> list) {
+    // Task 3
+    // метод, возвращающий список квадратов всех встречающихся
+    // только один раз элементов списка
+    public static List<Integer> getSquaresList(List<Integer> list) {
         return list.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
@@ -32,7 +37,10 @@ public class StreamApiMethods {
                 .toList();
     }
 
-    //Task 4
+    // Task 4
+    // метод, принимающий на вход коллекцию строк и возвращающий
+    // все строки, начинающиеся с заданной буквы, отсортированные по
+    // алфавиту
     public static List<String> getSortedStrings(Collection<String> list, String startLetter) {
         return list.stream()
                 .filter(n -> n.startsWith(startLetter))
@@ -40,21 +48,27 @@ public class StreamApiMethods {
                 .toList();
     }
 
-    //Task 5
+    // Task 5
+    // метод, принимающий на вход коллекцию и возвращающий её последний
+    // элемент или кидающий исключение, если коллекция пуста
     public static <T> T getLastElementOfCollection(Collection<T> collection) {
         return collection.stream()
                 .reduce((a, b) -> b)
                 .orElseThrow(() -> new NoSuchElementException("Collection is empty."));
     }
 
-    //Task 6
+    // Task 6
+    // метод, принимающий на вход массив целых чисел, возвращающий
+    // сумму чётных чисел или 0, если чётных чисел нет
     public static int getSumEvenNumber(int[] a) {
         return Arrays.stream(a)
                 .filter(n -> n % 2 == 0)
                 .reduce(0, Integer::sum);
     }
 
-    //Task 7
+    // Task 7
+    // метод, преобразовывающий все строки в списке в Map, где первый
+    // символ – ключ, оставшиеся – значение
     public static Map<Character, String> getStringMap(List<String> list) {
         return list.stream()
                 .collect(Collectors.toMap(str -> str.charAt(0), str -> str.substring(1)));
